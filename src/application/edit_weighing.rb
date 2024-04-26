2b629e1d0cc05587f26f0fc5c41d52b52174bbe0
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2024-04-26 15:22:32 -0300

    More

---
+++ src/application/edit_weighing.rb
@@ -0,0 +1,19 @@
+class Application::EditWeighing
+  def initialize(
+    weighing_entry_repository:
+  )
+    @weighing_entry_repository = weighing_entry_repository
+  end
+
+  def call(
+    id:,
+    date:,
+    weight_in_kg:
+  )
+    @weighing_entry_repository.edit(
+      id:,
+      date:,
+      weight_in_kg:
+    )
+  end
+end
